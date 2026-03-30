0b7b5cd2ee3f361efc7d9874be1ccb53ced8da4e
camilop <camiloperezpadilla7@gmail.com>
2026-03-30 15:49:10 -0500

    primer commit

---
+++ src/main/java/com/redpatitas/redPatitas/redPatitas.java
@@ -0,0 +1,13 @@
+package com.redpatitas.redPatitas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class redPatitas {
+
+	public static void main(String[] args) {
+		SpringApplication.run(redPatitas.class, args);
+	}
+
+}
